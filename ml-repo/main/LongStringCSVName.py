5920b5af8ced63dd9978b574f6df773526670ba0
mecha <v3suv1us@users.noreply.github.com>
2018-03-04 14:50:40 -0800

    fixing head, redoing csv so no spaces

--- ml-repo/main/LongStringCSVName.py
+++ ml-repo/main/LongStringCSVName.py
@@ -9,19 +9,19 @@ partOfWave = ["width Q", "width R", "width S", "width R prime", "width S prime",
 
 amplitude = ["amp JJ", "amp Q", "amp R", "amp S", "amp R prime", "amp S prime", "amp P", "amp T", "val QRSA", "val QRSTA"]
 
-onceStr = "Age, Sex, Height, Weight, QRS duration, P-R interval, Q-T interval, T interval, P interval, QRS, T, P, QRST, J, Heart rate, "
+onceStr = "Age,Sex,Height,Weight,QRS duration,P-R interval,Q-T interval,T interval,P interval,QRS,T,P,QRST,J,Heart rate,"
 
 finStr = ""
 
 for e in channels:
     for i in partOfWave:
-        finStr = finStr + e + " " + i + ", "
+        finStr = finStr + e + " " + i + ","
 
 for e in channels:
     for i in amplitude:
-        finStr = finStr + e + " " + i + ", "
+        finStr = finStr + e + " " + i + ","
 
-finStr = onceStr + finStr[:-2] + ", Identifier"
+finStr = onceStr + finStr[:-2] + ",Identifier"
 print(finStr)
 
 

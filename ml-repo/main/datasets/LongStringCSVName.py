89e40b7cb8ee77a1c1e48faa03e167ae79c3ef96
mecha <v3suv1us@users.noreply.github.com>
2018-03-04 14:23:23 -0800

    fixing csv & prog

--- ml-repo/main/datasets/LongStringCSVName.py
+++ ml-repo/main/datasets/LongStringCSVName.py
@@ -2,22 +2,29 @@ import numpy
 
 channels = ["DI", "DII", "AVR", "AFL", "V1", "V2", "V3", "V4", "V5", "V6"]
 
-partOfWave = ["Q", "R", "S", "R prime", "S prime"]
+partOfWave = ["width Q", "width R", "width S", "width R prime", "width S prime", "Num. of intrinsic deflections",
+              "Ragged R wave", "Diphasic Derivation of R wave",
+              "Ragged P wave", "Diphasic Derivation of P wave",
+              "Ragged T wave", "Diphasic Derivation of T wave"]
 
-amplitude = ["JJ", "Q", "R", "S", "R prime", "S prime", "P ", "T", "QRSA", "QRSTA"]
+amplitude = ["amp JJ", "amp Q", "amp R", "amp S", "amp R prime", "amp S prime", "amp P", "amp T", "val QRSA", "val QRSTA"]
+
+onceStr = "Age, Sex, Height, Weight, QRS duration, P-R interval, Q-T interval, T interval, P interval, QRS, T, P, QRST, J, Heart rate, "
 
 finStr = ""
 
 for e in channels:
     for i in partOfWave:
-        finStr = finStr + e + ": width " + i + ", "
+        finStr = finStr + e + " " + i + ", "
 
 for e in channels:
     for i in amplitude:
-        finStr = finStr + e + ": amp " + i + ", "
+        finStr = finStr + e + " " + i + ", "
 
-print(finStr[:-1])
+finStr = onceStr + finStr[:-2]
+print(finStr)
+
 
 text_file = open("Output.txt", "w")
-text_file.write("Purchase Amount: %s" % finStr[:-2])
+text_file.write(finStr)
 text_file.close()

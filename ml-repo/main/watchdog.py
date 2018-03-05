ee0b0b1e75e106d51c00d91e56534283e328fdf9
aadhi <V3SUV1US@users.noreply.github.com>
2018-03-04 16:55:56 -0800

    prepping data

--- ml-repo/main/watchdog.py
+++ ml-repo/main/watchdog.py
@@ -8,16 +8,23 @@ Created on Sun Mar  4 13:00:39 2018
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Imputer
+from sklearn.linear_model import SGDClassifier
 
 np.random.seed(42)
 csv_data = "https://raw.githubusercontent.com/V3SUV1US/ECG-WatchDog/master/ml-repo/main/datasets/arrythmia-ds.csv"
 
 def load_data():
-    return pd.read_csv(csv_data)
+    return pd.read_csv(csv_data, na_values= '?')
 ecg_data = load_data()
 #ecg_data.hist(bins=50, figsize=(20,15))
 #plt.save_fig("attribute_histogram_plots")
-ecg_data["Age"].hist()
+ecg_data["Height"].hist()
 plt.show()
-train_set, test_set = train_test_split(ecg_data, test_size=0.2, random_state=42)
+imputer= Imputer(strategy= "median")
+imputer.fit(ecg_data)
+ecg_datawithoutlabel = ecg_data.drop('Identifier', axis=1)
+ecg_datawithoutlabel.as_matrix()
+#x_train, y_train = ecg_datawithoutlabel, ecg_data['Identifier']
+#sgd_clf= SGDClassifier(random_state=42)
+#sgd_clf.fit(x_train,y_train)

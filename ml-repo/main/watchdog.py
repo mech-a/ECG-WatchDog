41dc67742e6df11de58467e47234a63ba81b3916
aadhi <V3SUV1US@users.noreply.github.com>
2018-03-10 19:07:42 -0800

    new changes

--- ml-repo/main/watchdog.py
+++ ml-repo/main/watchdog.py
@@ -6,13 +6,14 @@ Created on Sun Mar  4 13:00:39 2018
 """
 
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import Imputer
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix, precision_score, recall_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import precision_recall_curve
 
 np.random.seed(42)
 csv_data = "https://raw.githubusercontent.com/V3SUV1US/ECG-WatchDog/master/ml-repo/main/datasets/arrythmia-ds.csv"
@@ -45,12 +46,27 @@ dt_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
 curr_clf = sgd_clf
 
 
-curr_clf.fit(x_train, y_train)
+curr_clf.fit(x_train, y_train_1)
 
 y_train_predictions = cross_val_predict(curr_clf, x_train, y_train_1, cv=3)
+y_scores = cross_val_predict(curr_clf, x_train, y_train_1, cv=3, method="decision_function")
+precisions, recalls, thresholds = precision_recall_curve(y_train_1, y_scores)
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision", linewidth=2)
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall", linewidth=2)
+    plt.xlabel("Threshold", fontsize=16)
+    plt.legend(loc="upper left", fontsize=16)
+    plt.ylim([0, 1])
 
-confusionmat = confusion_matrix(y_train, y_train_predictions)
-recall = recall_score(y_train, y_train_predictions)
-precision = precision_score(y_train, y_train_predictions)
+plt.figure(figsize=(8, 4))
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+plt.xlim([-700000, 700000])
+plt.savefig("precision_recall_vs_threshold_plot")
+plt.show()
+y_train_pred_90 = (y_scores > -500000)
+
+#confusionmat = confusion_matrix(y_train, y_train_predictions)
+recall = recall_score(y_train_1, y_train_pred_90)
+precision = precision_score(y_train_1, y_train_pred_90)
 
 # we need high precision low recall
